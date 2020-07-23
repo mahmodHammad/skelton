a0d182d135af59af272c55ef5c03f68039d63c92
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-23 23:02:53 +0200

    getting rig absolute positions

--- src/components/Model.js
+++ src/components/Model.js
@@ -4,9 +4,10 @@ import { extractBones, loadModel, castShadow } from "./ModelLoader";
 
 function createModel(shift = 0) {
   loadModel().then((gltf) => {
+    // gltf.scene.scale.set(0.03,0.03,0.03)
     scene.add(gltf.scene);
     console.log(gltf)
-      
+    extractBones(gltf)
     render();
 
   });
--- src/components/ModelLoader.js
+++ src/components/ModelLoader.js
@@ -1,6 +1,8 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { scene, render } from "./setup.js";
 import filepath from "../model/dog.glb";
+import * as THREE from "three";
+
 console.log(filepath);
 var modelLoader = new GLTFLoader();
 function loadModel() {
@@ -21,65 +23,34 @@ function loadModel() {
 }
 
 function extractBones(gltf) {
-  const body =
-    gltf.scene.children[0].children[0].children[0].children[0].children[0]
-      .children[0].children[0].children[0];
+  const root =
+    gltf.scene.children[0].children[0]
+    const foot1 = root.children[0]
+    const body = root.children[1]
+    const foot2 = root.children[2]
+    const foot3 = root.children[3]
+    const foot4 = root.children[4]
+    console.log(root)
+    console.log(foot1)
+    console.log(foot2)
+    console.log(foot3)
+    console.log(foot4)
+    console.log(body)
+
+    scene.add( new THREE.BoxHelper( foot1, 0x00ffff ) );
+    scene.add( new THREE.BoxHelper( foot2, 0x00ff00 ) );
+    scene.add( new THREE.BoxHelper( foot3, 0x0000ff ) );
+    scene.add( new THREE.BoxHelper( foot4, 0xff00ff ) );
+    scene.add( new THREE.BoxHelper( body, 0xffff00 ) );
+    const box = new THREE.Box3;
+    box.setFromObject(body);
+    console.log("BBOOXX",box)
+
   ///////////////////////////////////////////////////////////////// Upper body
-  const upperBody = body.children[0].children[0];
-  const neck = upperBody.children[0];
-  const head = neck.children[0];
-  const head_sensor = head.children[0];
-  ///////////////////////////////////////////////////////////////// Left Leg
-  const leg_l = body.children[1];
-  const knee_l = leg_l.children[0];
-  const shin_l = knee_l.children[0];
-  const foot_l = shin_l.children[0];
-  const toe_l = foot_l.children[0].children[0];
-  ///////////////////////////////////////////////////////////////// Right Leg
-  const leg_r = body.children[2];
-  const knee_r = leg_r.children[0];
-  const shin_r = knee_r.children[0];
-  const foot_r = shin_r.children[0];
-  const toe_r = foot_r.children[0].children[0];
-  ///////////////////////////////////////////////////////////////// Left Arm
-  const arm_l = upperBody.children[1];
-  const arm_l_bi = arm_l.children[0];
-  const arm_l_elbow = arm_l_bi.children[0];
-  const forearm_l = arm_l_elbow.children[0];
-  const hand_l = forearm_l.children[0].children[0];
-  ///////////////////////////////////////////////////////////////// Right Arm
-  const arm_r = upperBody.children[2];
-  const arm_r_bi = arm_r.children[0];
-  const arm_r_elbow = arm_r_bi.children[0];
-  const forearm_r = arm_r_elbow.children[0];
-  const hand_r = forearm_r.children[0].children[0];
+
 
   return {
     body,
-    upperBody,
-    neck,
-    head,
-    head_sensor,
-    leg_l,
-    knee_l,
-    shin_l,
-    foot_l,
-    toe_l,
-    leg_r,
-    knee_r,
-    shin_r,
-    foot_r,
-    toe_r,
-    arm_l,
-    arm_l_bi,
-    arm_l_elbow,
-    hand_l,
-    arm_r,
-    arm_r_bi,
-    arm_r_elbow,
-    forearm_r,
-    hand_r,
-    forearm_l,
   };
 }
 
