096b88f13f2ff9d134837232931ed1327c4115aa
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-25 16:40:28 +0200

    toggle sidebar, enhance UI of toggle btn

--- src/UI/Sidebar.js
+++ src/UI/Sidebar.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import skelton from "../variables/skelton";
 export default function Sidebar() {
+    const [toggle, settoggle] = useState(true)
   return (
-    <div id="sidebar" className="sidebar">
+    <div id="sidebar" className={`sidebar ${toggle?"openSB":"closeSB"}`}>
       <ul className="sidebar-list">
         {skelton.map((s) => (
           <li onClick={e=>console.log(e.target.id)} id={s.id}>{s.label}</li>
         ))}
       </ul>
 
-      <button id="toggle"> > </button>
+      <button id="toggle" onClick={()=>settoggle(!toggle)}> {toggle? ">" : "<"} </button>
     </div>
   );
 }
