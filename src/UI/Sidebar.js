a2c228a11975b507612b9bdd9141da0ee03a615e
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-25 17:33:57 +0200

    add bottom bar/ rotation animation for toggle btn

--- src/UI/Sidebar.js
+++ src/UI/Sidebar.js
@@ -1,16 +1,37 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import skelton from "../variables/skelton";
 export default function Sidebar() {
-    const [toggle, settoggle] = useState(true)
+  const [sideToggle, setsideToggle] = useState(true);
+  const [bottomToggle, setbottomToggle] = useState(true);
   return (
-    <div id="sidebar" className={`sidebar ${toggle?"openSB":"closeSB"}`}>
-      <ul className="sidebar-list">
-        {skelton.map((s) => (
-          <li onClick={e=>console.log(e.target.id)} id={s.id}>{s.label}</li>
-        ))}
-      </ul>
+    <div>
+      <div id="sidebar" className={`sidebar ${sideToggle ? "openSB" : ""}`}>
+        <ul className="sidebar-list">
+          {skelton.map((s) => (
+            <li onClick={(e) => console.log(e.target.id)} id={s.id}>
+              {s.label}
+            </li>
+          ))}
+        </ul>
 
-      <button  className="toggle" id="toggle" onClick={()=>settoggle(!toggle)}> {toggle? ">" : "<"} </button>
+        <button
+          className={`Toggle  ${sideToggle?"rotateSideToggle":""}`}
+          id="sideToggle"
+          onClick={() => setsideToggle(!sideToggle)}
+        >
+          {"<"}
+        </button>
+      </div>
+      <div className={`bottombar ${bottomToggle? "openBB" : ""}`}>
+        <button
+          className={`Toggle ${bottomToggle?"rotateBottomToggle":""}`}
+          id="bottomToggle"
+          onClick={() => setbottomToggle(!bottomToggle)}
+        >
+        {">"}
+        </button>
+          Hello world my name is Mahmoud Hammad i'am 21 years old
+      </div>
     </div>
   );
 }
