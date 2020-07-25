9f32f0eb28782f4686eddd2810f08801251bc7de
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-25 15:10:04 +0200

    update Project description/ mount the sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Cat} from './components/Cat';
+import Sidebar from './UI/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
       <Cat/>
+    <Sidebar/>
     </div>
   );
 }
